0656658ace2da69ccd3213bcc56f391185c4ffec
ajay812998 <ajay812998@gmail.com>
2020-10-11 00:00:50 +0530

    Added : If Tie occur then continue till difference is min 2

--- flipcoin.sh
+++ flipcoin.sh
@@ -13,16 +13,49 @@ random=$((RANDOM%2))
        ((countHeads++))
 if [ $countHeads -eq 21 ]
 then
-    echo "Head Wins or occuring times is :" $(($countHeads-$countTails))
+    Headdifference=$(($countHeads-$countTails))
+    echo "Head Wins or occuring times is :" $Headdifference
     break
 fi
 else
     ((countTails++))
 if [ $countTails -eq 21 ]
 then
-     echo "Tail Wins or occuring times is :" $(($countTails-$countHeads))
+     Taildifference=$(($countTails-$countHeads))
+     echo "Tail Wins or occuring times is :" $Taildifference
      break
 fi
 fi
 done
+echo $Headdifference
+echo $taildifference
+
+#tie condition
+
+if [ $Headdifference -eq 0 ] || [ $Taildifference -eq 0 ]
+then
+while [ $Headdifference -gt 2 ] || [ $Taildifference -gt 2 ]
+do
+random=$((RANDOM%2))
+   if [ $random -eq 0 ]
+   then
+       ((countHeads++))
+if [ $countHeads -gt 2 ]
+then
+       Headdifference=$(($countHeads-$countTails))
+       echo "First time Head or Tail is same, Now Head Wins by :" $Headdifference
+       break
+fi
+       else
+       ((countTails++))
+       if [ $countTails -gt 2 ]
+       then
+       Taildifference=$(($countTails-$countHeads))
+echo "First time Head or Tail is same, Now Tail Wins by :" $Taildifference
+       break
+fi
+fi
+done
+fi
+
 
