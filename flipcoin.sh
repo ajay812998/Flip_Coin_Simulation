207e8ce2bc31b07d19b4dd38608c2ae281c42507
ajay812998 <ajay812998@gmail.com>
2020-10-10 22:52:25 +0530

    Added : Heads or Tails winner

--- flipcoin.sh
+++ flipcoin.sh
@@ -1,3 +1,15 @@
 #!/bin/bash -x
 
 #This problem displays the winner Heads or Tails
+
+countHeads=0
+countTails=0
+
+random=$((RANDOM%2))
+if [ $random -eq 0 ]
+then
+     echo "Head is winner"
+else
+     echo "Tail is winner"
+fi
+
